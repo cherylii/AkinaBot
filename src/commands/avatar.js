db8a7516420b70422bcf0460ae6d7c559bf9533a
Choon How <choonhow.20@ichat.sp.edu.sg>
2021-08-22 22:02:10 +0800

    can now get avatar without pinging target

--- src/commands/avatar.js
+++ src/commands/avatar.js
@@ -4,7 +4,7 @@ module.exports = {
 	name: 'avatar',
 	aliases: ['icon', 'pfp'],
 	description: 'Get the profile picture of a user! they must\'ve a really good one...',
-	usage: { "<\@user>": "User that you want to get the picture of" },
+	usage: { "<\@user>": "User that you want to get the picture of. Can also put userid" },
 	subcommands: {},
 	category: "Information",
 	args: false,
@@ -12,6 +12,16 @@ module.exports = {
 	guildOnly: false,
 	execute(client, message, args, currency, category, distube, tmpMsg) {
 
+		if ((message.mentions.users.size == 1) || args[0].match(/^\d{18}$/)) {
+			let user = message.mentions.users.first() || client.users.cache.get(args[0])
+			avatarEmbed = new Discord.MessageEmbed()
+				.setColor(colorHex)
+				.setAuthor(`${user.id.toString() === "776735927229087764" ? "Your cutest bot" : user.username}'s profile avatar`, user.displayAvatarURL({ dynamic: true }))
+				.setDescription(`**[${user.id.toString() === "776735927229087764" ? "Here's a link to see me even more clearly!" : "Here's a link to their avatar!"}](${user.displayAvatarURL({ dynamic: true })})**`)
+				.setImage(user.displayAvatarURL({ dynamic: true, size: 1024 }))
+				.setFooter(`${user.id.toString() === "776735927229087764" ? "Hehe, It's the cutest thing ever!" : "Isn't it beautifully pristine?"}`, user.displayAvatarURL({ dynamic: true }))
+			return message.channel.send({ content: `${user.id.toString() === "776735927229087764" ? "*Hehe, you wish to see me more clearly? <3*" : ' '}`, embeds: [avatarEmbed] });
+		}
 		if (!message.mentions.users.size) {
 			avatarEmbed = new Discord.MessageEmbed()
 				.setColor(colorHex)
@@ -20,16 +30,6 @@ module.exports = {
 				.setImage(message.author.displayAvatarURL({ dynamic: true, size: 1024 }))
 				.setFooter(`Isn't it beautifully pristine?`, message.author.displayAvatarURL({ dynamic: true }))
 			return message.channel.send({ embeds: [avatarEmbed] });
-		}
-		if (message.mentions.users.size == 1) {
-			let user = message.mentions.users.first()
-			avatarEmbed = new Discord.MessageEmbed()
-				.setColor(colorHex)
-				.setAuthor(`${user.id.toString() === "776735927229087764" ? "Your cutest bot" : user.username}'s profile avatar`, user.displayAvatarURL({ dynamic: true }))
-				.setDescription(`**[${user.id.toString() === "776735927229087764" ? "Here's a link to see me even more clearly!" : "Here's a link to their avatar!"}](${user.displayAvatarURL({ dynamic: true })})**`)
-				.setImage(user.displayAvatarURL({ dynamic: true, size: 1024 }))
-				.setFooter(`${user.id.toString() === "776735927229087764" ? "Hehe, It's the cutest thing ever!" : "Isn't it beautifully pristine?"}`, user.displayAvatarURL({ dynamic: true }))
-			return message.channel.send({ content: `${ user.id.toString() === "776735927229087764" ? "*Hehe, you wish to see me more clearly? <3*" : ' '}`, embeds: [avatarEmbed] });
 		}
 		if (message.mentions.users.size > 1 && message.mentions.users.size < 11) {
 			avatarEmbed = new Discord.MessageEmbed()
